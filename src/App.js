8d145a5f62a847502976f89f1079c1b63121ff4d
김소연 <soyeon.dev@gmail.com>
2021-10-27 22:52:56 +0900

    사진첩 로드

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import api from "./api/theCatAPI.js";
+import Node from './components/Node.js';
 
 export default class App {
-    
     constructor($target){
         console.log("app in");
         const loadRootData = async () => {
+            const nodes = document.createElement("div");
             let response;
             response = await api.fetchRoot();
             console.log("app response: ", response);
+            response.map((item, idx) => {
+                console.log("item: ", item);
+                console.log("idx: ", idx);
+                const node = new Node($target, item);
+
+            });
         };
         loadRootData();
 
 
-        // const node = new Node($target);
+
     }
 }
--- src/components/Node.js
+++ src/components/Node.js
@@ -1,5 +1,11 @@
+// import fileImg from "../../assets/file.png";
+// import directoryImg from "../../assets/directory.png";
+
 export default class Node {
-    constructor({$target}){
+    constructor($target, data){
+        this.data = data;
+        this.$target = $target;
+        console.log("node data: ", data);
         this.div = document.createElement("div");
         this.div.className = "Node";
         $target.appendChild(this.div);
@@ -7,6 +13,19 @@ export default class Node {
     }
 
     render(){
-
+        const nodeImg = document.createElement("img");
+        console.log("this.data.type: ", this.data.type);
+        nodeImg.width = '116';
+        nodeImg.height = '116';
+        if(this.data.type == "DIRECTORY"){
+            nodeImg.src = "../../assets/directory.png";
+        } else {
+            nodeImg.src = "../../assets/file.png";
+        }
+        const name = document.createElement("div");
+        name.innerHTML = this.data.name;
+        
+        this.div.appendChild(nodeImg);
+        this.div.appendChild(name);
     }
 }
